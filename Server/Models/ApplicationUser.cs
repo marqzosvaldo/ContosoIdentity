53d34f6178d54c492a85794eed414dec431b77d2
marqzosvaldo <crosser.xr@gmail.com>
2020-07-29 00:42:46 -0500

    LogIn, Logout and Registered

---
+++ Server/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Contoso.Server.Models {
+    public class ApplicationUser : IdentityUser { }
+}
